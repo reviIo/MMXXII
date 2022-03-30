521b7e1e031bac2467fa50ffbeee512e46872b1c
reviIo <86659859+reviIo@users.noreply.github.com>
2022-03-31 11:46:14 +1300

    Add files via upload

---
+++ playback.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+################## The FPS averages around 15 atm, needs to be fine-tuned ##################
+
+import os
+import sys
+
+import cv2
+import rospy
+import signal
+import subprocess
+import matplotlib.pyplot as plt
+
+from datetime import datetime
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+from matplotlib.animation import FuncAnimation
+
+frame_num = 1
+fps_list = []
+
+bagfile_folder = os.path.join(os.getcwd(), "recording/")
+
+print(os.listdir(bagfile_folder))
+
+try:
+    bag_filename = input("Enter the name of the bag file (please include .bag at the end): ")
+    while os.path.isfile("%s%s" %(bagfile_folder, bag_filename))==False:
+        print("Invalid filename.")
+        bag_filename = input("Enter the name of the bag file (please include .bag at the end): ")
+except EOFError as e:
+    print(e)
+    bag_filename = "two_in_one.bag"
+
+full_path = "%s%s" %(bagfile_folder, bag_filename)
+
+command = subprocess.Popen(['rosbag', 'play', full_path], cwd=os.getcwd())
+
+rospy.init_node("eavesdropper", anonymous=True)
+
+def grab_rgb_frame():
+    rgb_msg = rospy.wait_for_message("/camera/color/image_raw", Image)
+
+    try:
+        rgb_img = CvBridge()
+        rgb_img = rgb_img.imgmsg_to_cv2(rgb_msg, "rgb8")
+    except CvBridgeError as e:
+        print(e)
+
+    return rgb_img
+
+def grab_depth_frame():
+    depth_msg = rospy.wait_for_message("/camera/depth/image_rect_raw", Image)
+
+    try:
+        depth_img = CvBridge()
+        depth_img = depth_img.imgmsg_to_cv2(depth_msg, "8UC1")
+    except CvBridgeError as e:
+        print(e)
+    
+    return depth_img
+
+#create two subplots
+fig = plt.figure(figsize=(12,8))
+ax1 = plt.subplot(1,2,1)
+ax2 = plt.subplot(1,2,2)
+
+#create two image plots
+rgb_img = ax1.imshow(grab_rgb_frame())
+depth_img = ax2.imshow(grab_depth_frame())
+
+def update(i):
+    global frame_num, fps_list
+
+    start_time = datetime.now()
+
+    rgb_img.set_data(grab_rgb_frame())
+    depth_img.set_data(grab_depth_frame())
+
+    process_time = datetime.now() - start_time
+    print("Frame Number %d" % frame_num)
+    fps = 1/process_time.total_seconds()
+    print("FPS = %d" % (fps))
+    fps_list.append(fps)
+
+    frame_num += 1
+    
+ani = FuncAnimation(fig, update, interval=0)
+
+def close(event):
+    if event.key == 'q':
+        plt.close(event.canvas.figure)
+
+cid = fig.canvas.mpl_connect("key_press_event", close)
+
+plt.show()
+
+os.killpg(os.getpgid(command.pid), signal.SIGTERM)
+
+#################################################################################################################
+
+'''cont = True
+
+rospy.init_node("eavesdropper", anonymous=True)
+
+try:
+    while cont:
+        start_time = datetime.now()
+        
+        rgb_msg = rospy.wait_for_message("/camera/color/image_raw", Image)
+        depth_msg = rospy.wait_for_message("/camera/depth/image_rect_raw", Image)
+
+        try:
+            rgb_img = CvBridge()
+            rgb_img = rgb_img.imgmsg_to_cv2(rgb_msg, "rgb8")
+            depth_img = CvBridge()
+            depth_img = depth_img.imgmsg_to_cv2(depth_msg, "8UC1")
+        except CvBridgeError as e:
+            print(e)
+
+        cv2.imshow("Window Name", rgb_img)
+        cv2.imshow("Window Mane", depth_img)
+        #key = cv2.waitKey(1)
+
+        process_time = datetime.now() - start_time
+        print("Frame Number %d" % frame_num)
+        fps = 1/process_time.total_seconds()
+        print("FPS = %d" % (fps))
+        fps_list.append(fps)
+
+        frame_num += 1
+
+        if cv2.waitKey(1) == 27:
+            cont = False
+except:
+    os.killpg(os.getpgid(command.pid), signal.SIGTERM)
+    sys.exit()
+os.killpg(os.getpgid(command.pid), signal.SIGTERM)'''
