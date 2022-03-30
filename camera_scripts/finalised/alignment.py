572645cc56f6306b3e782f8bf2cefab196c27b38
reviIo <86659859+reviIo@users.noreply.github.com>
2022-03-31 11:48:28 +1300

    Add files via upload

---
+++ camera_scripts/finalised/alignment.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import cv2                                # state of the art computer vision algorithms library
+import numpy as np                        # fundamental package for scientific computing
+import matplotlib.pyplot as plt           # 2D plotting library producing publication quality figures
+import pyrealsense2 as rs                 # Intel RealSense cross-platform open-source API
+print("Environment Ready")
+
+
+
+
+# Setup:
+pipe = rs.pipeline()
+cfg = rs.config()
+cfg.enable_device_from_file("/home/marvin/catkin_ws/src/camera_scripts/finalised/recording/row1_2022-01-12-12-54-49.bag")
+profile = pipe.start(cfg)
+
+# Skip 5 first frames to give the Auto-Exposure time to adjust
+for x in range(5):
+  pipe.wait_for_frames()
+  
+# Store next frameset for later processing:
+frameset = pipe.wait_for_frames()
+color_frame = frameset.get_color_frame()
+depth_frame = frameset.get_depth_frame()
+
+# Cleanup:
+pipe.stop()
+print("Frames Captured")
+
+
+
+
+color = np.asanyarray(color_frame.get_data())
+plt.rcParams["axes.grid"] = False
+plt.rcParams['figure.figsize'] = [12, 6]
+plt.imshow(color)
+
+
+
+
+colorizer = rs.colorizer()
+colorized_depth = np.asanyarray(colorizer.colorize(depth_frame).get_data())
+plt.imshow(colorized_depth)
+
+
+
+
+
+
+# Create alignment primitive with color as its target stream:
+align = rs.align(rs.stream.color)
+frameset = align.process(frameset)
+
+# Update color and depth frames:
+aligned_depth_frame = frameset.get_depth_frame()
+colorized_depth = np.asanyarray(colorizer.colorize(aligned_depth_frame).get_data())
+
+# Show the two frames together:
+images = np.hstack((color, colorized_depth))
+plt.imshow(images)
+
+
+
+
+
+
